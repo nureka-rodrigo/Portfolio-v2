6567c720beb3d42d1c6af504dfdf0268a884664d
Nureka Rodrigo <cst20069@std.uwu.ac.lk>
2024-07-27 22:25:37 +0530

    Create EmailSection.jsx and SocialLinks.jsx

---
+++ src/components/EmailSection.jsx
@@ -0,0 +1,18 @@
+const EmailSection = () => {
+  return (
+    <div className="hidden lg:flex lg:flex-col lg:items-center fixed right-8 bottom-0 mb-4">
+      <div className="flex flex-col items-center">
+        <a
+          href="mailto:nurekadulshanmc@gmail.com"
+          className="text-gray-700 dark:text-gray-300 hover:text-yellow-500 dark:hover:text-yellow-600 transition duration-300"
+          style={{ writingMode: "vertical-rl", transform: "rotate(3600deg)" }}
+        >
+          nurekadulshanmc@gmail.com
+        </a>
+        <div className="w-px bg-gray-500 dark:bg-gray-300 h-24 mt-4 transition duration-300"></div>
+      </div>
+    </div>
+  );
+};
+
+export default EmailSection;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,11 +14,19 @@ const NavItems = [
   },
   {
     text: "About",
-    link: "/about",
+    link: "#about",
+  },
+  {
+    text: "Experience",
+    link: "#experience",
+  },
+  {
+    text: "Projects",
+    link: "#projects",
   },
   {
     text: "Contact",
-    link: "/contact",
+    link: "#contact",
   },
 ];
 
@@ -29,7 +37,7 @@ const Header = () => {
 
   return (
     <>
-      <header className={`transition duration-500 ${mobileMenuOpen ? "bg-opacity-50" : ""}`}>
+      <header className={`transition duration-300 ${mobileMenuOpen ? "bg-opacity-50" : ""}`}>
         <nav
           className="mx-auto flex max-w-7xl items-center justify-between p-6 gap-x-8"
           aria-label="Global"
@@ -37,7 +45,7 @@ const Header = () => {
           <div className="flex lg:flex-1">
             <Link to="/" className="-m-1.5 p-1.5">
               <img
-                className={`h-10 w-auto transition duration-500 ${currentTheme === "dark" ? "filter invert" : ""}`}
+                className={`h-10 w-auto transition duration-300 ${currentTheme === "dark" ? "filter invert" : ""}`}
                 src="/logo.svg"
                 alt="Brand"
               />
@@ -49,7 +57,7 @@ const Header = () => {
               className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
               onClick={() => setMobileMenuOpen(true)}
             >
-              <IoMenu className="h-6 w-6 text-neutral-950 dark:text-gray-100 transition duration-500" aria-hidden="true"/>
+              <IoMenu className="h-6 w-6 text-neutral-950 dark:text-gray-100 transition duration-300" aria-hidden="true"/>
             </button>
           </div>
           <Popover.Group className="hidden lg:flex lg:gap-x-12">
@@ -57,7 +65,7 @@ const Header = () => {
               <Link
                 key={index}
                 to={navItem.link}
-                className={`text-sm font-semibold leading-6 transition duration-500 ${
+                className={`text-sm font-semibold leading-6 transition duration-300 ${
                   location.pathname === navItem.link
                     ? "text-yellow-600 dark:text-yellow-500 font-bold"
                     : "text-gray-900 dark:text-gray-300"
@@ -96,7 +104,7 @@ const Header = () => {
               >
                 <div className="flex items-center justify-between">
                   <Link to="/" className="-m-1.5 p-1.5">
-                    <img className={`h-10 w-auto transition duration-500 ${currentTheme === "dark" ? "filter invert" : ""}`}
+                    <img className={`h-10 w-auto transition duration-300 ${currentTheme === "dark" ? "filter invert" : ""}`}
                          src="/logo.svg" alt=""/>
                   </Link>
                   <button
@@ -104,7 +112,7 @@ const Header = () => {
                     className="rounded-md text-gray-700"
                     onClick={() => setMobileMenuOpen(false)}
                   >
-                    <IoMdClose className="h-6 w-6 text-neutral-950 dark:text-gray-100 transition duration-500"/>
+                    <IoMdClose className="h-6 w-6 text-neutral-950 dark:text-gray-100 transition duration-300"/>
                   </button>
                 </div>
                 <div className="mt-6 flow-root">
@@ -114,7 +122,7 @@ const Header = () => {
                         <Link
                           key={index}
                           to={navItem.link}
-                          className={`-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 transition duration-500 ${
+                          className={`-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 transition duration-300 ${
                             location.pathname === navItem.link
                               ? "text-yellow-600 dark:text-yellow-500 font-bold"
                               : "text-gray-900 dark:text-gray-300"
