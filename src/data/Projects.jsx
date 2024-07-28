cbeb77e8791669f0e3a74d7711c06726b6709a92
Nureka Rodrigo <cst20069@std.uwu.ac.lk>
2024-07-28 13:29:17 +0530

    Create project data

---
+++ src/data/Projects.jsx
@@ -0,0 +1,37 @@
+export const Projects = [
+    {
+        title: "Portfolio",
+        description: "Modern, responsive, and user-friendly portfolio website that showcases my skills, projects, and experience.",
+        tags: ["ReactJS", "Tailwind CSS"],
+        github: "https://github.com/nureka-rodrigo/Portfolio-v1",
+        live: null,
+    },
+    {
+        title: "ABC University",
+        description: "Explore a comprehensive Student Management System designed to streamline administrative tasks and enhance the academic experience.",
+        tags: ["ReactJS", "Tailwind CSS", "Python", "Django",],
+        github: "https://github.com/nureka-rodrigo/abc-university-frontend",
+        live: null,
+    },
+    {
+        title: "Railway Management System",
+        description: "Revolutionizing railway operations with a robust system that manages schedules, bookings, and passenger information seamlessly.",
+        tags: ["HTML5", "CSS3", "JavaScript", "PHP", "MySQL", "Bootstrap",],
+        github: "https://github.com/nureka-rodrigo/SLRMS",
+        live: null,
+    },
+    {
+        title: "Yummy Kitchen",
+        description: "Discover and share delightful and revolutionary recipes on Yummy Kitchen, a user-friendly platform which connects food enthusiasts from all around the world to share recipes.",
+        tags: ["HTML5", "CSS3", "JavaScript", "Java", "JSP", "Servlet", "MySQL", "Bootstrap",],
+        github: "https://github.com/nureka-rodrigo/Yummy-Kitchen",
+        live: null,
+    },
+    {
+        title: "ABC Hotel",
+        description: "Simple, innovative and modern Hotel Management System that simplifies the process of managing hotel reservations and provides a seamless experience for guests.",
+        tags: ["HTML5", "CSS3", "JavaScript", "PHP", "MySQL", "Bootstrap"],
+        github: "https://github.com/nureka-rodrigo/ABC-Hotel",
+        live: null,
+    },
+]
