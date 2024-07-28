df0df6a3892c26cf080bf5a799547cbacb7b3d60
Nureka Rodrigo <cst20069@std.uwu.ac.lk>
2024-07-28 13:10:09 +0530

    Remove @headlessui/react

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,7 @@
 import {IoMdClose} from "react-icons/io";
 import {IoMenu} from "react-icons/io5";
-import {Dialog, Popover} from "@headlessui/react";
-import {useContext, useState} from "react";
 import {AnimatePresence, motion} from "framer-motion";
+import {useContext, useState} from "react";
 import ThemeButton from "./ThemeButton.jsx";
 import {ThemeContext} from "../providers/ThemeProvider.jsx";
 import {Link, useLocation} from "react-router-dom";
@@ -38,7 +37,7 @@ const Header = () => {
   return (
     <>
       <header
-        className={`fixed top-0 left-0 right-0 z-50 bg-gray-100 dark:bg-neutral-950 transition duration-300 ${mobileMenuOpen ? "bg-opacity-50" : ""}`}
+        className={`bg-gray-100 dark:bg-neutral-950 transition duration-300 ${mobileMenuOpen ? "bg-opacity-50" : "fixed top-0 left-0 right-0 z-50 bg-opacity-0"}`}
       >
         <nav
           className="mx-auto flex max-w-7xl items-center justify-between p-6 gap-x-8"
@@ -65,7 +64,7 @@ const Header = () => {
               />
             </button>
           </div>
-          <Popover.Group className="hidden lg:flex lg:gap-x-12">
+          <div className="hidden lg:flex lg:gap-x-12">
             {NavItems.map((navItem, index) => (
               <Link
                 key={index}
@@ -79,7 +78,7 @@ const Header = () => {
                 {navItem.text}
               </Link>
             ))}
-          </Popover.Group>
+          </div>
 
           <div className="pl-6 pr-2 hidden lg:block">
             <ThemeButton />
@@ -87,21 +86,16 @@ const Header = () => {
         </nav>
         <AnimatePresence>
           {mobileMenuOpen && (
-            <Dialog
-              as="div"
-              className="lg:hidden"
-              open={mobileMenuOpen}
-              onClose={() => setMobileMenuOpen(false)}
-            >
+            <>
               <motion.div
                 className="fixed inset-0 z-10 bg-black bg-opacity-50"
                 initial={{ opacity: 0 }}
                 animate={{ opacity: 1 }}
                 exit={{ opacity: 0 }}
                 transition={{ duration: 0.3 }}
+                onClick={() => setMobileMenuOpen(false)}
               />
-              <Dialog.Panel
-                as={motion.div}
+              <motion.div
                 className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-gray-100 dark:bg-neutral-950 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10"
                 initial={{ x: "100%" }}
                 animate={{ x: 0 }}
@@ -125,7 +119,7 @@ const Header = () => {
                   </button>
                 </div>
                 <div className="mt-6 flow-root">
-                  <div className="-my-6 divide-y divide-gray-500/10">
+                  <div className="-my-6 divide-y divide-gray-500">
                     <div className="space-y-2 py-6">
                       {NavItems.map((navItem, index) => (
                         <Link
@@ -147,8 +141,8 @@ const Header = () => {
                     </div>
                   </div>
                 </div>
-              </Dialog.Panel>
-            </Dialog>
+              </motion.div>
+            </>
           )}
         </AnimatePresence>
       </header>
