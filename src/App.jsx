cdc53eb5957e2aa9b89c894b334544097b632373
Nureka Rodrigo <cst20069@std.uwu.ac.lk>
2024-08-01 10:10:15 +0530

    Reorganize imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import useInViewAnimation from "./hooks/useInViewAnimation";
-import Home from "./components/sections/Home.jsx";
-import Header from "./components/layout/Header.jsx";
 import About from "./components/sections/About.jsx";
-import Experience from "./components/sections/Experience.jsx";
-import Projects from "./components/sections/Projects.jsx";
 import Contact from "./components/sections/Contact.jsx";
-import Footer from "./components/layout/Footer.jsx";
 import EmailSection from "./components/common/EmailSection.jsx";
+import Experience from "./components/sections/Experience.jsx";
+import Footer from "./components/layout/Footer.jsx";
+import Header from "./components/layout/Header.jsx";
+import Home from "./components/sections/Home.jsx";
+import Projects from "./components/sections/Projects.jsx";
 import ScrollToTopButton from "./components/common/ScrollToTopButton.jsx";
 import SocialLinks from "./components/common/SocialLinks.jsx";
+import useInViewAnimation from "./hooks/useInViewAnimation";
 import "./App.css";
 
 function App() {
